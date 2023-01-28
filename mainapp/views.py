74dd636180fbf6c3de2ebd7e602339c49f8ffb64
EgorTreglazov <egort.123@yandex.ru>
2023-01-28 15:50:52 +0500

    connecting HTML and CSS

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class IndexPageView(TemplateView):
+    template_name = 'mainapp/index.html'
+
+
+class AboutPageView(TemplateView):
+    template_name = 'mainapp/about.html'
+
+
+class ContactPageView(TemplateView):
+    template_name = 'mainapp/contact.html'
+
+
+class CoursesPageView(TemplateView):
+    template_name = 'mainapp/courses.html'
+
+
+class PortfolioPageView(TemplateView):
+    template_name = 'mainapp/portfolio.html'
+
+
+class PricingPageView(TemplateView):
+    template_name = 'mainapp/pricing.html'
