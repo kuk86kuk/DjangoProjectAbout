4fd57c75d13a9e45335aa94e0973fa74f1d5ecab
EgorTreglazov <egort.123@yandex.ru>
2023-01-30 00:14:05 +0500

    templates are made

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -7,10 +7,10 @@ app_name = MainappConfig.name
 
 urlpatterns = [
     path("", views.IndexPageView.as_view()),
-    path("index/", views.IndexPageView.as_view()),
-    path("about/", views.AboutPageView.as_view()),
-    path("contact/", views.ContactPageView.as_view()),
-    path("courses/", views.CoursesPageView.as_view()),
-    path("portfolio/", views.PortfolioPageView.as_view()),
-    path("pricing/", views.PricingPageView.as_view()),
+    path("index/", views.IndexPageView.as_view(), name='index'),
+    path("about/", views.AboutPageView.as_view(), name='about'),
+    path("contact/", views.ContactPageView.as_view(), name='contact'),
+    path("courses/", views.CoursesPageView.as_view(), name='courses'),
+    path("portfolio/", views.PortfolioPageView.as_view(), name='portfolio'),
+    path("pricing/", views.PricingPageView.as_view(), name='pricing'),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -2,8 +2,18 @@ from django.http import HttpResponse
 from django.views.generic import TemplateView
 
 
+
 class IndexPageView(TemplateView):
     template_name = 'mainapp/index.html'
+    test = 123
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        
+        context['news_title'] = 'Новости'
+        context['news_data'] = 'Описание новости'
+        
+        return context
+
 
 
 class AboutPageView(TemplateView):
